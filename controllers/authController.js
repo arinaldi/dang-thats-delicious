8bad2ad63790ece129d2489e8852cb5b2520337d
Tony <arinaldi@vt.edu>
2017-06-04 16:43:33 -0500

    Add search functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -38,7 +39,14 @@ exports.forgot = async (req, res) => {
   await user.save();
 
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `Check your email for a password reset link. ${resetURL}`);
+  await mail.send({
+    user,
+    subject: 'Password reset',
+    resetURL,
+    filename: 'password-reset'
+  });
+
+  req.flash('success', `Check your email for a password reset link`);
   res.redirect('/login');
 };
 
